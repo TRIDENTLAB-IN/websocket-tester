467d82feaef131ce2cbca062268891a8bac12325
Aaryadev <devmgbhat@gmail.com>
2020-10-13 02:15:27 +0530

    Github link

--- assets/js/wt.js
+++ assets/js/wt.js
@@ -190,3 +190,11 @@ function snackbar(txt){
   snackbarContainer.MaterialSnackbar.showSnackbar(data);
 
 }
+
+const shell = require('electron').shell;
+
+// assuming $ is jQuery
+$(document).on('click', 'a[href^="http"]', function(event) {
+    event.preventDefault();
+    shell.openExternal(this.href);
+});
