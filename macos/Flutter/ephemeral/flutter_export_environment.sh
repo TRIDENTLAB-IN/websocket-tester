0ec7da2a3d4d1d824f4ec4f6d4528d3753908321
Aaryadev <devmgbhat@gmail.com>
2026-04-07 15:38:38 +0530

    for better dev moved to flutter instead  nodejs

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/aaryadev/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/aaryadev/WDTTB/DEVELOPMENT/Projects/Trident/websocket-tester"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
